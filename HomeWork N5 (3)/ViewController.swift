284ffa5e50f00d64c7d05401ff6f69e7615b8e6f
Magdiel <magdiel.altynbekov@gmail.com>
2022-12-10 17:37:30 +0600

    Initial Commit

---
+++ HomeWork N5 (3)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork N5 (3)
+//
+//  Created by Magdiel Altynbekov on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
